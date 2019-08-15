bc177c4ce6edfb2a468f6e3d18abe67274838c0e
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2019-08-15 16:54:09 +0200

    Whiteboard challenges in correct order

--- cookie_clicker/competitions/impl.py
+++ cookie_clicker/competitions/impl.py
@@ -5,9 +5,12 @@ from cookie_clicker.utils.decorators import *
 from cookie_clicker.clicker_state import ClickerState
 
 
-@register_competition(skip=False)
-def revenue_at_timestep(clicker_state: ClickerState) -> float:
-    return clicker_state.total_cookies
+@register_competition(skip=False, bigger_is_better=False, duration=10e28)
+def time_to_revenue(clicker_state: ClickerState):
+    for timestep, _, _, revenue in clicker_state.history:
+        if revenue > 1e42:
+            return timestep
+    return math.sqrt(sys.float_info.max)
 
 
 @register_competition(skip=False)
@@ -16,13 +19,10 @@ def profit_at_timestep(clicker_state: ClickerState) -> float:
 
 
 @register_competition(skip=False)
+def revenue_at_timestep(clicker_state: ClickerState) -> float:
+    return clicker_state.total_cookies
+
+
+@register_competition(skip=False)
 def cps_at_timestep(clicker_state: ClickerState) -> float:
     return clicker_state.cps
-
-
-@register_competition(skip=False, bigger_is_better=False, duration=10e28)
-def time_to_revenue(clicker_state: ClickerState):
-    for timestep, _, _, revenue in clicker_state.history:
-        if revenue > 1e42:
-            return timestep
-    return math.sqrt(sys.float_info.max)
