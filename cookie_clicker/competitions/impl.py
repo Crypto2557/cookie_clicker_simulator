050d5dc066d6de809a529b393a138270f3ff2fc8
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2019-08-15 16:43:48 +0200

    Added time to revenue challenge

--- cookie_clicker/competitions/impl.py
+++ cookie_clicker/competitions/impl.py
@@ -1,3 +1,6 @@
+import sys
+import math
+
 from cookie_clicker.utils.decorators import *
 from cookie_clicker.clicker_state import ClickerState
 
@@ -11,6 +14,15 @@ def revenue_at_timestep(clicker_state: ClickerState) -> float:
 def profit_at_timestep(clicker_state: ClickerState) -> float:
     return clicker_state.current_cookies
 
+
 @register_competition(skip=False)
 def cps_at_timestep(clicker_state: ClickerState) -> float:
     return clicker_state.cps
+
+
+@register_competition(skip=False, bigger_is_better=False, duration=10e28)
+def time_to_revenue(clicker_state: ClickerState):
+    for timestep, _, _, revenue in clicker_state.history:
+        if revenue > 1e42:
+            return timestep
+    return math.sqrt(sys.float_info.max)
