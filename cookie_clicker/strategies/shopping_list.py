6d4626bee8f071ce4637b6521003f7599781e9ae
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2019-08-15 14:15:13 +0200

    Fixed missing type hinting, added save/load

--- cookie_clicker/strategies/shopping_list.py
+++ cookie_clicker/strategies/shopping_list.py
@@ -1,22 +1,35 @@
 """ Shopping List strategy """
 import pathlib
+from typing import List, Callable, Union
 from cookie_clicker.utils.decorators import register_strategy
 
 
-def get_shopping_list_strategy_from_file(shopping_list_path):
+def load_purchases_file(shopping_list_path: str) -> List[str]:
     with open(shopping_list_path) as shopping_list:
         purchases = [str(x).strip() for x in shopping_list]
 
-    return get_shopping_list_strategy_from_list(purchases)
+    return purchases
 
 
-def get_shopping_list_strategy_from_list(purchases):
+def save_purchases_file(shopping_list_path: str, purchases: List[str]) -> None:
+    with open(shopping_list_path, 'w') as shopping_list:
+        shopping_list.write('\n'.join(purchases) + '\n')
+
+
+def get_shopping_list_strategy_from_file(shopping_list_path: str) -> Callable:
+    return get_shopping_list_strategy_from_list(load_purchases_file(shopping_list_path))
+
+
+def get_shopping_list_strategy_from_list(purchases: List[str]) -> Callable:
     purchases = purchases.copy()
 
-    def shopping_list_inner(cookies, cps, time_left, building_info):
+    def shopping_list_inner(cookies, cps, time_left, building_info) -> Union[str, None]:
         if len(purchases) > 0:
             purchase = purchases.pop(0)
-            return purchase
+            if purchase == 'None':
+                return None
+            else:
+                return purchase
         else:
             return None
 
@@ -25,6 +38,6 @@ def get_shopping_list_strategy_from_list(purchases):
 
 """ Register lists """
 for item in pathlib.Path("shopping_lists").iterdir():
-    strategy = get_shopping_list_strategy_from_file(item)
+    strategy = get_shopping_list_strategy_from_file(str(item))
     strategy.__name__ = item.name
     register_strategy(skip=False)(strategy)
