a46fd0fd2f3cd39a86d4e433b37815ca7e598a24
Clemens-Alexander Brust <clemens-alexander.brust@uni-jena.de>
2019-08-16 08:46:21 +0200

    Added time to Mathematician achievement

--- cookie_clicker/competitions/impl.py
+++ cookie_clicker/competitions/impl.py
@@ -3,6 +3,11 @@ import math
 
 from cookie_clicker.utils.decorators import *
 from cookie_clicker.clicker_state import ClickerState
+import cookie_clicker.building_info as building_info
+import cookie_clicker.utils as utils
+
+
+FOREVER = math.sqrt(sys.float_info.max)
 
 
 @register_competition(skip=False, bigger_is_better=False, duration=10e28)
@@ -10,7 +15,7 @@ def time_to_revenue(clicker_state: ClickerState):
     for timestep, _, _, revenue in clicker_state.history:
         if revenue > 1e42:
             return timestep
-    return math.sqrt(sys.float_info.max)
+    return FOREVER
 
 
 @register_competition(skip=False)
@@ -26,3 +31,18 @@ def revenue_at_timestep(clicker_state: ClickerState) -> float:
 @register_competition(skip=False)
 def cps_at_timestep(clicker_state: ClickerState) -> float:
     return clicker_state.cps
+
+
+@register_competition(skip=False, bigger_is_better=False, duration=10e20)
+def time_to_mathematician(clicker_state: ClickerState) -> float:
+    my_building_info = building_info.BuildingInfo(utils.Config.DEFAULT_BUILDING_INFO)
+    pairs = sorted([(building, my_building_info.get_cost(building=building)) for building in my_building_info.buildings], key=lambda x: x[1], reverse=True)
+    required_counts = {building: min(128, int(math.pow(2.0, i))) for i, (building, _) in enumerate(pairs)}
+
+    for timestep, item_name, _, _ in clicker_state.history:
+        if item_name in required_counts.keys():
+            required_counts[item_name] -= 1
+            if all([required <= 0 for _, required in required_counts.items()]):
+                return timestep
+
+    return FOREVER
