c830f5b16e11a3643565b3df62de22a2cbdbd192
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2019-08-19 11:09:43 +0200

    fixed variable name

--- tests/main.py
+++ tests/main.py
@@ -4,8 +4,8 @@ if __name__ != '__main__': raise Exception("Do not import me!")
 import sys
 from os.path import abspath, dirname, join
 
-addons_dir = abspath(join(abspath(dirname(__file__)), ".."))
-sys.path.insert(0, addons_dir)
+project_dir = abspath(join(abspath(dirname(__file__)), ".."))
+sys.path.insert(0, project_dir)
 
 ### Import tests like this:
 # from tests.[test_module] import *
